95974c66501d8141a22a4f07b54334d28a6a77ef
unknown <hrayrashchyan94@gmail.com>
2024-07-10 18:57:12 +0400

    requie header for orders

--- resources/views/orders/index.php
+++ resources/views/orders/index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once __DIR__ . "/../layout/header.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Orders</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-
-<body>
 <div class="container">
     <h1>Orders</h1>
     <table class="table">
